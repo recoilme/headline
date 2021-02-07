ed6d4b55bec9b0c783d1a58a3f718eeba55ee75f
recoilme <vadim-kulibaba@yandex.ru>
2021-02-07 21:28:02 +0300

    remove rss

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const DEFAULT_CORS_PROXY = url => `https://cors.zserge.com/?u=${encodeURIComponent(url)}`;
 
 const DEFAULT_FEEDS = [
-  'https://news.google.com/rss',
   'https://recostream.go.mail.ru/?n=6&stream_id=lenta_pulse_mail_ru&query_type=recommends',
-  'https://www.reddit.com/r/golang.rss',
   'https://recostream.go.mail.ru/?source_filter_id=8283545368347858465&use_orig_imgs=1&title_length=150&ver=1.103.0&n=12&stream_id=source_only&query_type=recommends',
 ];
 
